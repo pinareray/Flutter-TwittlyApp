98260e5bbf782263546791662387caa91208a79c
pinareray <pinareray3@gmail.com>
2025-01-22 13:16:55 +0300

    added app bar, tab bar, card, list

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -14,14 +15,30 @@ class _HomeViewState extends State<HomeView> {
   late ScrollController scrollController;
   String _randomImage = "https://picsum.photos/200/300";
   String fakeMessage = "When you realize how precious and fragile life is, it changes your whole perspective.";
+  bool isHeaderClose = false;
+  double lastOffset = 0;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     scrollController = ScrollController();
 
-    scrollController.addListener((){print(scrollController.offset);});
+    scrollController.addListener(() {
+      if (scrollController.offset <= 0) {
+        isHeaderClose = false;
+      } else {
+        isHeaderClose = scrollController.offset > lastOffset ? true : false;
+      }
+      setState(() {
+        lastOffset = scrollController.offset;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    scrollController.dispose();
   }
 
   @override
@@ -31,59 +48,135 @@ class _HomeViewState extends State<HomeView> {
       body: SafeArea(
         child: DefaultTabController(
           length: defaultTabLength,
-           child: Column(
+          child: Column(
             children: [
-              _appBar, 
+              _containerAppbar,
               _tabbarItems,
-             _expandedListView,
-             ],
-         ),
+              _expandedListView,
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget get _fabButton => FloatingActionButton(
-    onPressed: (){}, child: const Icon(Icons.send),);
-
-  Widget get _appBar => AppBar(
-    title: _appBarItems,
-  );
-  Widget get _appBarItems => Wrap(
-    crossAxisAlignment: WrapCrossAlignment.center,
-    spacing: 10,
-    children: [
-      CircleAvatar(backgroundImage: NetworkImage(_photoUrl)),
-      Text("Home", style: titleTextStyle)],
+        onPressed: () {},
+        child: const Icon(Icons.send),
       );
 
-  Widget get _tabbarItems => const TabBar(tabs: <Widget> [
-            Tab (icon: Icon(Icons.home),),
-            Tab (icon: Icon(Icons.search),),
-            Tab (icon: Icon(Icons.notifications),),
-            Tab (icon: Icon(Icons.message),),
-   ],
-  );
+  Widget get _appBar => AppBar(
+        elevation: 0,
+        centerTitle: false,
+        title: _appBarItems,
+      );
 
-  Widget get _expandedListView => Expanded(child: _listView,);
+  Widget get _containerAppbar => AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        height: isHeaderClose ? 0 : 50,
+        child: _appBar,
+      );
+
+  Widget get _appBarItems => Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          CircleAvatar(backgroundImage: NetworkImage(_photoUrl)),
+          SizedBox(width: 10),
+          Text("Home", style: titleTextStyle),
+        ],
+      );
+
+  Widget get _tabbarItems => const TabBar(tabs: <Widget>[
+        Tab(
+          icon: Icon(Icons.home),
+        ),
+        Tab(
+          icon: Icon(Icons.search),
+        ),
+        Tab(
+          icon: Icon(Icons.notifications),
+        ),
+        Tab(
+          icon: Icon(Icons.message),
+        ),
+      ]);
+
+  Widget get _expandedListView => Expanded(child: _listView);
 
   Widget get _listView => ListView.builder(
-    itemCount: 10,
-    controller: ScrollController(),
-    itemBuilder: (context , index){
-    return _listViewCard;
- });
+        itemCount: 10,
+        controller: scrollController,
+        itemBuilder: (context, index) {
+          return _listViewCard;
+        },
+      );
 
-  Widget get _listViewCard => Card(
-    child: ListTile(
-      leading: CircleAvatar(backgroundImage: NetworkImage(_randomImage),),
-      title: Wrap(children: [
-        Text("@niemand", style: titleTextStyle,),
-        Text(fakeMessage),
-      ],),
+ Widget get _listViewCard => Card(
+  child: ListTile(
+    contentPadding: EdgeInsets.all(8),
+    title: Row(
+      children: [
+        CircleAvatar(
+          backgroundImage: NetworkImage(_randomImage),
+          radius: 20,
+        ),
+        SizedBox(width: 10),
+        Text("@niemand", style: titleTextStyle),
+      ],
     ),
-  );
+    subtitle: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 8),
+        Text(fakeMessage),
+        SizedBox(height: 10),
+        _placeHolderField,
+        _textButtonList,
+      ],
+    ),
+  ),
+);
+
+
+  Widget _listCardTitle(String text) => Text(
+        text,
+        style: titleTextStyle,
+      );
+
+  Widget get _placeHolderField => Container(
+        height: 100,
+        child: Placeholder(),
+      );
+
+  Widget get _textButtonList => Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _iconLabelButton,
+          _iconLabelButton,
+          _iconLabelButton,
+          _iconLabelButton
+        ],
+      );
+
+  Widget _iconLabel(String text) => Wrap(
+        spacing: 5,
+        children: [
+          Icon(
+            Icons.favorite,
+            color: CupertinoColors.inactiveGray,
+          ),
+          Text(text),
+        ],
+      );
+
+  Widget get _iconLabelButton => InkWell(
+        child: _iconLabel("1"),
+        onTap: () {},
+      );
 }
 
-final titleTextStyle = 
-TextStyle(fontSize: 20, fontWeight: FontWeight.w800, color: Colors.black,);
+final titleTextStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w800,
+  color: Colors.black,
+);
